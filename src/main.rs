73e83beaf0e93dfcba3ff1a3feac5e704c918fcb
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2025-01-19 01:16:44 +0500

    basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::path::Path;
+use net_trace::*;
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("./example/pcap/rsasnakeoil2.pcap");
+    let res = match parse_pcap(path) {
+        Ok(res) => res,
+        Err(e) => {
+            println!("{:#?}", e);
+            return;
+        }
+    };
+    println!("{:#?}", res);
 }
